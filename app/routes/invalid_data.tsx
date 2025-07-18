182c471cf553c6fcfcc804759988d8b61c379880
Emir Bosnian <emiraga@gmail.com>
2025-07-19 00:30:35 +0200

    missed two files

---
+++ app/routes/invalid_data.tsx
@@ -0,0 +1,43 @@
+import MainToolbar from "~/toolbar/toolbar";
+import type { Route } from "./+types/index";
+import type { OutletContext } from "~/data/types";
+import { useOutletContext } from "react-router";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Invalid Data" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function InvalidData() {
+  const { invalidData } = useOutletContext<OutletContext>();
+
+  return (
+    <main>
+      <MainToolbar />
+      <section className="block mx-4">
+        <h3 className="font-serif text-4xl m-4">
+          Encountered invalid data: ({invalidData.length})
+        </h3>
+
+        {invalidData.length === 0 ? (
+          <p>No errors to display.</p>
+        ) : (
+          <ul>
+            {invalidData.map((error, index) => (
+              <li key={index} className="mb-2 p-2 border rounded">
+                <p className="font-bold">{error.message}</p>
+                {error.details && (
+                  <pre className="mt-2 p-2 rounded ">
+                    {JSON.stringify(error.details, null, 2)}
+                  </pre>
+                )}
+              </li>
+            ))}
+          </ul>
+        )}
+      </section>
+    </main>
+  );
+}
