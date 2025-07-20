8618567b0dd0f2febd16f7628d8ccf229e194a20
Emir Bosnian <emiraga@gmail.com>
2025-07-20 09:21:09 +0200

    Submenu on hover

--- app/toolbar/toolbar.tsx
+++ app/toolbar/toolbar.tsx
@@ -28,23 +28,6 @@ export const MainToolbarNoOutlet: React.FC<{
   invalidData,
 }) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const dropdownRef = useRef<HTMLLIElement>(null);
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (
-        dropdownRef.current &&
-        !dropdownRef.current.contains(event.target as Node)
-      ) {
-        setIsDropdownOpen(false);
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [dropdownRef]);
 
   let styleSelected =
     "block mx-1 py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500";
@@ -113,8 +96,14 @@ export const MainToolbarNoOutlet: React.FC<{
             {list.map((item, i) => {
               if (item.isDropdown) {
                 return (
-                  <li key={i} className="relative" ref={dropdownRef}>
-                    <button
+                  <li
+                    key={i}
+                    className="relative"
+                    onMouseEnter={() => setIsDropdownOpen(true)}
+                    onMouseLeave={() => setIsDropdownOpen(false)}
+                  >
+                    <Link
+                      to="/chars"
                       id="dropdownNavbarLink"
                       data-dropdown-toggle="dropdownNavbar"
                       className={`flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent ${
@@ -122,7 +111,6 @@ export const MainToolbarNoOutlet: React.FC<{
                           ? styleSelected
                           : styleInactive
                       }`}
-                      onClick={() => setIsDropdownOpen(!isDropdownOpen)}
                     >
                       {item.name}
                       <svg
@@ -140,21 +128,20 @@ export const MainToolbarNoOutlet: React.FC<{
                           d="m1 1 4 4 4-4"
                         />
                       </svg>
-                    </button>
+                    </Link>
                     {isDropdownOpen && (
                       <div
                         id="dropdownNavbar"
                         className="z-10 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600 absolute"
                       >
                         <ul
-                          className="py-2 text-sm text-gray-700 dark:text-gray-400"
+                          className="p-2 text-sm text-gray-700 dark:text-gray-400"
                           aria-labelledby="dropdownLargeButton"
                         >
                           <li>
                             <NavLink
                               to="/chars_sentence_input"
                               className={`${styleInactive} block px-4 py-2`}
-                              onClick={() => setIsDropdownOpen(false)}
                             >
                               Sentence input
                             </NavLink>
@@ -163,7 +150,6 @@ export const MainToolbarNoOutlet: React.FC<{
                             <NavLink
                               to="/todo_chars"
                               className={`${styleInactive} block px-4 py-2`}
-                              onClick={() => setIsDropdownOpen(false)}
                             >
                               Todo chars
                             </NavLink>
@@ -172,7 +158,6 @@ export const MainToolbarNoOutlet: React.FC<{
                             <NavLink
                               to="/props"
                               className={`${styleInactive} block px-4 py-2`}
-                              onClick={() => setIsDropdownOpen(false)}
                             >
                               All props
                             </NavLink>
@@ -181,7 +166,6 @@ export const MainToolbarNoOutlet: React.FC<{
                             <NavLink
                               to="/chars"
                               className={`${styleInactive} block px-4 py-2`}
-                              onClick={() => setIsDropdownOpen(false)}
                             >
                               All chars
                             </NavLink>
@@ -190,7 +174,6 @@ export const MainToolbarNoOutlet: React.FC<{
                             <NavLink
                               to="/chars_multiple_pronunciation"
                               className={`${styleInactive} block px-4 py-2`}
-                              onClick={() => setIsDropdownOpen(false)}
                             >
                               Multiple pronounciation
                             </NavLink>
