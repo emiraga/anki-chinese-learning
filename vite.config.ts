0e60425f0939620854c19a684d2e5639f5ebcce3
Emir Bosnian <emiraga@gmail.com>
2025-09-16 16:06:42 +0800

    enable react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import babel from "vite-plugin-babel";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 const external = [
@@ -17,7 +18,14 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     reactRouter(),
-    tsconfigPaths()
+    babel({
+      filter: /\.tsx?$/,
+      babelConfig: {
+        presets: ["@babel/preset-typescript"],
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+    tsconfigPaths(),
   ],
   build: {
     rollupOptions: {
@@ -30,7 +38,7 @@ export default defineConfig({
   server: {
     fs: {
       // Allow serving files from outside the project root for dev tools
-      allow: ['..'],
+      allow: [".."],
     },
   },
 });
