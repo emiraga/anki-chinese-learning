60f12b5d3737ec120df4f6a2e7197a64cd06cd0e
Emir Bosnian <emiraga@gmail.com>
2026-09-15 09:56:43 +0200

    fixes

--- utils/video/cut_video_by_subtitles.py
+++ utils/video/cut_video_by_subtitles.py
@@ -59,7 +59,11 @@ Used commands:
     /Users/emirb/In\ Progress\ Temporary/you\ are\ the\ apple\ of\ my\ eye/You\ are\ the\ Apple\ of\ My\ Eye\ 2011\ 1080p\ x264.en.srt \
     --padding-start 0.3 --padding-end 0.6 --reencode --overlap-min 0.1 --anki-prefix apple_of_my_eye_ --webm
 
-    cd "/Users/emirb/In Progress Temporary/you are the apple of my eye" && python3 -m http.server 8080
+    ./utils/video/serve_media.py --directory "/Users/emirb/In Progress Temporary/you are the apple of my eye"
+
+    (Use serve_media.py rather than `python3 -m http.server`: the latter ignores
+    Range requests, which makes the clips non-seekable in the Anki card and
+    breaks the trimDurationStart seek.)
 """
 
 import argparse
